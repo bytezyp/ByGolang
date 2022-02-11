765eedec8681cb615ed31e2764f539a641012d0e
zhangyupeng <zhangyupeng@qutoutiao.net>
2022-02-11 10:38:40 +0800

    update file

--- BySlice/readme.go
+++ BySlice/readme.go
@@ -5,7 +5,18 @@ import (
 	"time"
 )
 
+func modifySlice(innerSlice []string) {
+	innerSlice = append(innerSlice, "a")
+	innerSlice[0] = "b"
+	innerSlice[1] = "b"
+	fmt.Println(innerSlice, len(innerSlice),cap(innerSlice))
+}
 func main() {
+	outerSlice:= make([]string, 0, 3)
+	outerSlice = append(outerSlice, "a", "a")
+	modifySlice(outerSlice)
+	fmt.Println(outerSlice,len(outerSlice),cap(outerSlice))
+	return
 	var ints []int
 	fmt.Printf("ints : %v , %p \n", ints, ints)
 	// ints : [] , 0x0
